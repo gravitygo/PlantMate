47e3e27d6952ecf9c96469f5c19308e84e0eddfa
gravitygo <chyle.andrei.lee@gmail.com>
2023-03-30 00:31:53 +0800

    Changed top nav from going to activity home to just finishing the current activity

--- app/src/main/java/com/plantmate/plantmate/fragments/FragmentTopNav.kt
+++ app/src/main/java/com/plantmate/plantmate/fragments/FragmentTopNav.kt
@@ -1,13 +1,10 @@
 package com.plantmate.plantmate.fragments
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.plantmate.plantmate.HomeActivity
 import com.plantmate.plantmate.R
 import com.plantmate.plantmate.databinding.FragmentTopNavBinding
 
@@ -19,7 +16,7 @@ class FragmentTopNav(val color: Int): Fragment(R.layout.fragment_top_nav){
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentTopNavBinding.inflate(inflater)
 
         binding.topPanel.setBackgroundColor(color)
