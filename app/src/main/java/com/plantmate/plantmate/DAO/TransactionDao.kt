fde622ee25cce5a0bd3a02502acd5f15d6f7f86c
gravitygo <chyle.andrei.lee@gmail.com>
2023-04-10 22:34:56 +0800

    Done on Charts, just need the transaction recycler view at the bottom

--- app/src/main/java/com/plantmate/plantmate/DAO/TransactionDao.kt
+++ app/src/main/java/com/plantmate/plantmate/DAO/TransactionDao.kt
@@ -12,28 +12,18 @@ import kotlin.random.Random
 class TransactionDao {
 
     val db = FirebaseFirestore.getInstance()
-    var min = Float.MAX_VALUE
-    var max = Float.MIN_VALUE
     var mAuth = FirebaseAuth.getInstance()
 
-    fun read(size: Long, callback: EntryCallback) {
+    fun read(col: String,callback: EntryCallback) {
         val entries = mutableListOf<Map<String, Any>>()
-        var runningSum=0f
         db.collection("users")
             .document("${mAuth.uid}")
-            .collection("transaction")
+            .collection(col)
             .orderBy("date")
-            .limit(size)
             .get()
             .addOnSuccessListener { querySnapshot ->
                 querySnapshot.documents.forEach{doc ->
                     entries.add(doc.data!!)
-                    val cost = doc.data!!["cost"] as Number
-                    runningSum += cost.toFloat()
-                    if(min > runningSum)
-                        min = runningSum
-                    if(max<runningSum)
-                        max = runningSum
                 }
                 callback.onSuccess(entries)
             }
--- app/src/main/java/com/plantmate/plantmate/fragments/FragmentChart.kt
+++ app/src/main/java/com/plantmate/plantmate/fragments/FragmentChart.kt
@@ -2,16 +2,19 @@ package com.plantmate.plantmate.fragments
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.security.identity.CredentialDataResult.Entries
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
@@ -19,18 +22,18 @@ import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.listener.ChartTouchListener
 import com.github.mikephil.charting.listener.OnChartGestureListener
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
-import com.github.mikephil.charting.utils.EntryXComparator
 import com.google.firebase.Timestamp
+import com.google.type.DateTime
 import com.plantmate.plantmate.CallBack.EntryCallback
-import com.plantmate.plantmate.DAO.DataHelper.Companion.data
 import com.plantmate.plantmate.DAO.DataHelper.Companion.dataGenerate
 import com.plantmate.plantmate.DAO.TransactionDao
 import com.plantmate.plantmate.R
 import com.plantmate.plantmate.databinding.FragmentChartBinding
+import java.sql.Time
 import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.random.Random
+
 
 class FragmentChart : Fragment(R.layout.fragment_chart)  {
     private lateinit var binding: FragmentChartBinding
@@ -39,11 +42,19 @@ class FragmentChart : Fragment(R.layout.fragment_chart)  {
     private lateinit var btnInventory : ConstraintLayout
     private lateinit var btnPurchase : ConstraintLayout
     private lateinit var btnWithered : ConstraintLayout
-    private lateinit var lineChart: LineChart
-    private lateinit var documents: List<Map<String, Any>>
 
+    private lateinit var btn1W : Button
+    private lateinit var btn1M : Button
+    private lateinit var btn3M : Button
+    private lateinit var btn1Y : Button
+    private lateinit var btnAll : Button
+
+    private lateinit var lineChart: LineChart
+    private lateinit var documents: MutableList<Map<String, Any>>
+    var chartsData: Array<Array<MutableList<Entry>>> = Array(5) { Array(5) { mutableListOf() } }
+    private var currType: Int = 0
+    private var currSize: Int = 0
     val td = TransactionDao()
-    // TODO: Each transaction should have product_id, product_type, transaction_Type{1-4}, amount, transaction cost, transaction_net cost
     @Suppress("DEPRECATION")
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -57,37 +68,76 @@ class FragmentChart : Fragment(R.layout.fragment_chart)  {
         btnInventory = binding.fragmentChartBtnInventory
         btnPurchase = binding.fragmentChartBtnPurchase
         btnWithered = binding.fragmentChartBtnWithered
+        btn1W = binding.fragmentChartBtn1w
+        btn1M = binding.fragmentChartBtn1m
+        btn3M = binding.fragmentChartBtn3m
+        btn1Y = binding.fragmentChartBtn1y
+        btnAll = binding.fragmentChartBtnAll
+        chartDataGet()
+
+        lineChart = binding.lineChart
+        setupLineChart()
 
         btnNetProfit.setOnClickListener{
             removeBtn(btnNetProfit)
+            currType=0
             binding.fragmentChartIvBackground.setBackgroundColor(resources.getColor(R.color.primary))
+
+            setData(chartsData[currType][currSize])
         }
         btnSale.setOnClickListener{
             removeBtn(btnSale)
+            currType=1
             binding.fragmentChartIvBackground.setBackgroundColor(resources.getColor(R.color.yellow_primary))
+
+            setData(chartsData[currType][currSize])
         }
         btnInventory.setOnClickListener{
             removeBtn(btnInventory)
+            currType=2
             binding.fragmentChartIvBackground.setBackgroundColor(resources.getColor(R.color.purple_primary))
+
+            setData(chartsData[currType][currSize])
         }
         btnPurchase.setOnClickListener{
             removeBtn(btnPurchase)
+            currType=3
             binding.fragmentChartIvBackground.setBackgroundColor(resources.getColor(R.color.orange_primary))
+
+            setData(chartsData[currType][currSize])
         }
         btnWithered.setOnClickListener{
             removeBtn(btnWithered)
+            currType=4
             binding.fragmentChartIvBackground.setBackgroundColor(resources.getColor(R.color.red_primary))
-        }
-        lineChart = binding.lineChart
-        setupLineChart()
-        // Add data to the line chart
-        dataGet(7)
 
-        binding.fragmentChartBtn1w.setOnClickListener { dataGet(7)}
-        binding.fragmentChartBtn1m.setOnClickListener { dataGet(28) }
-        binding.fragmentChartBtn3m.setOnClickListener { dataGet(84) }
-        binding.fragmentChartBtn1y.setOnClickListener { dataGet(365) }
-        binding.fragmentChartBtnAll.setOnClickListener { dataGet(10000) }
+            setData(chartsData[currType][currSize])
+        }
+        btn1W.setOnClickListener {
+            removeTimeLine(btn1W)
+            currSize=0
+            setData(chartsData[currType][currSize])
+        }
+        btn1M.setOnClickListener {
+            removeTimeLine(btn1M)
+            currSize=1
+            setData(chartsData[currType][currSize])
+        }
+        btn3M.setOnClickListener {
+            removeTimeLine(btn3M)
+            currSize=2
+            setData(chartsData[currType][currSize])
+        }
+        btn1Y.setOnClickListener {
+            removeTimeLine(btn1Y)
+            currSize=3
+            setData(chartsData[currType][currSize])
+        }
+        btnAll.setOnClickListener {
+            removeTimeLine(btnAll)
+            currSize=4
+            setData(chartsData[currType][currSize])
+        }
 
         return binding.root
     }
@@ -95,7 +145,15 @@ class FragmentChart : Fragment(R.layout.fragment_chart)  {
         button.setBackgroundResource(R.drawable.round_rectangle)
         button.elevation = 9.0F
     }
-
+    private fun selectTimeLine(button: Button){
+        button.setBackgroundResource(R.drawable.chart_fill_gradient)
+    }
+    private fun removeTimeLine(button: Button){
+        listOf(btn1W, btn1M, btn3M, btn1Y, btnAll).forEach {
+            it.setBackgroundResource(R.color.transparent)
+        }
+        selectTimeLine(button)
+    }
     private fun removeBtn(button: ConstraintLayout) {
         listOf(btnNetProfit, btnSale, btnInventory, btnPurchase, btnWithered).forEach {
             it.setBackgroundResource(0)
@@ -142,13 +200,15 @@ class FragmentChart : Fragment(R.layout.fragment_chart)  {
 
         lineChart.setOnChartValueSelectedListener(object: OnChartValueSelectedListener {
             override fun onValueSelected(e: Entry?, h: Highlight?) {
-                val current = DecimalFormat("#,##0.00").format(h?.y)
+                if(h!=null){
+                    val current = DecimalFormat("#,##0.00").format(h.y)
 
-                binding.fragmentChartTvValue.text = current.substring(0, current.length-3)
-                binding.fragmentChartTvDecimal.text = current.substring(current.length-3, current.length)
-                binding.fragmentChartTvAllTimeIncrease.text = "${h?.x}"
-                val time = documents[h?.x!!.toInt()]["date"] as Timestamp
-                binding.fragmentChartTvDate.text = "${getDateWithoutTime(time.toDate())}"
+                    binding.fragmentChartTvValue.text = current.substring(0, current.length-3)
+                    binding.fragmentChartTvDecimal.text = current.substring(current.length-3, current.length)
+                    val time = documents[h.x!!.toInt()]["date"] as Timestamp
+                    binding.fragmentChartTvDate.text = getDateWithoutTime(time.toDate())
+                    binding.fragmentChartTvAllTimeIncrease.text = "${h.x}"
+                }
             }
 
             override fun onNothingSelected() {
@@ -160,15 +220,17 @@ class FragmentChart : Fragment(R.layout.fragment_chart)  {
         lineChart.onChartGestureListener = object : OnChartGestureListener {
             override fun onChartGestureStart(me: MotionEvent?, lastPerformedGesture: ChartTouchListener.ChartGesture?) {
                 val h = lineChart.getHighlightByTouchPoint(me?.x ?: 0F, me?.y ?: 0F)
-                val current = DecimalFormat("#,##0.00").format(h?.y)
+                if(h!=null){
+                    val current = DecimalFormat("#,##0.00").format(h.y)
 
-                binding.fragmentChartAllTime.visibility = View.GONE
-                binding.fragmentChartTvDate.visibility = View.VISIBLE
-                binding.fragmentChartTvValue.text = current.substring(0, current.length-3)
-                binding.fragmentChartTvDecimal.text = current.substring(current.length-3, current.length)
-                val time = documents[h?.x!!.toInt()]["date"] as Timestamp
-                binding.fragmentChartTvDate.text = "${getDateWithoutTime(time.toDate())}"
-                binding.fragmentChartTvAllTimeIncrease.text = "${h?.x}"
+                    binding.fragmentChartAllTime.visibility = View.GONE
+                    binding.fragmentChartTvDate.visibility = View.VISIBLE
+                    binding.fragmentChartTvValue.text = current.substring(0, current.length-3)
+                    binding.fragmentChartTvDecimal.text = current.substring(current.length-3, current.length)
+                    val time = documents[h.x!!.toInt()]["date"] as Timestamp
+                    binding.fragmentChartTvDate.text = getDateWithoutTime(time.toDate())
+                    binding.fragmentChartTvAllTimeIncrease.text = "${h.x}"
+                }
             }
 
 
@@ -186,37 +248,124 @@ class FragmentChart : Fragment(R.layout.fragment_chart)  {
             }
 
             override fun onChartLongPressed(me: MotionEvent?) {}
-
             override fun onChartDoubleTapped(me: MotionEvent?) {}
-
             override fun onChartFling(me1: MotionEvent?, me2: MotionEvent?, velocityX: Float, velocityY: Float) {}
-
             override fun onChartScale(me: MotionEvent?, scaleX: Float, scaleY: Float) {}
-
             override fun onChartTranslate(me: MotionEvent?, dX: Float, dY: Float) {}
-
             override fun onChartSingleTapped(me: MotionEvent?) {}
         }
 
     }
-    private fun dataGet(size: Long){
-        td.read(size, object:EntryCallback{
-            override fun onSuccess(datas: List<Map<String, Any>>) {
-                documents=datas
-                val entries = mutableListOf<Entry>()
+    fun chartDataGet(){
+        td.read("aggregate", object: EntryCallback{
+            override fun onSuccess(values: List<Map<String, Any>>) {
+                documents= mutableListOf()
+                var lastVal = Array(5){0f}
+                var week = Calendar.getInstance()
+                var month= Calendar.getInstance()
+                var month3= Calendar.getInstance()
+                var year= Calendar.getInstance()
+                week.add(Calendar.WEEK_OF_YEAR, -1)
+                month.add(Calendar.MONTH,-1)
+                month3.add(Calendar.MONTH, -3)
+                year.add(Calendar.YEAR, -1)
 
-                for((i, data) in datas.withIndex()){
-                    val cost = data["cost"] as Number
-                    entries.add(Entry(i.toFloat(), cost.toFloat()))
+                var dateTimeline = arrayOf<Calendar>(
+                    week,
+                    month,
+                    month3,
+                    year
+                )
+
+                var previousDate: Date = year.time
+                previousDate.seconds=0
+                previousDate.minutes=0
+                previousDate.hours=0
+
+                var index=0
+                for(value in values){
+                    val dateStamp = value["date"] as Timestamp
+                    val date = dateStamp.toDate()
+                    val netProfit = value["netprofit"] as Number
+                    val sales = value["sales"] as Number
+                    val purchases = value["purchases"] as Number
+                    val inventory = value["inventory"] as Number
+                    val propagation = value["propagation"] as Number
+                    date.seconds=0
+                    date.minutes=0
+                    date.hours=0
+                    date.time = date.time/1000*1000
+
+                    if(previousDate>date)
+                        previousDate=date
+
+                    val increaser = Calendar.getInstance()
+
+
+                    increaser.time=Date(previousDate.time / 1000 * 1000)
+                    while(previousDate<date){
+                        val default = mapOf<String, Any>(
+                            "date" to Timestamp(previousDate),
+                            "netprofit" to 0f,
+                            "sales" to 0f,
+                            "purchases" to 0f,
+                            "inventory" to 0f,
+                            "propagation" to 0f
+                        )
+                        for(j in 0 .. 4){
+                            chartsData[j][4].add(Entry(index.toFloat(), lastVal[j]))
+
+                            for(k in 0 .. 3) {
+                                if (dateTimeline[k].time <= previousDate) {
+                                    chartsData[j][k].add(
+                                        Entry(
+                                            index.toFloat(),
+                                            lastVal[j]
+                                        )
+                                    )
+                                }
+                            }
+                        }
+                        documents.add(index, default)
+
+                        increaser.add(Calendar.DATE, 1)
+                        previousDate=increaser.time
+                        index++
+                    }
+                    lastVal[0]+=netProfit.toFloat()
+                    lastVal[1]+=sales.toFloat()
+                    lastVal[2]+=purchases.toFloat()
+                    lastVal[3]+=inventory.toFloat()
+                    lastVal[4]+=propagation.toFloat()
+                    for(j in 0 .. 4){
+                        chartsData[j][4].add(Entry(index.toFloat(), lastVal[j]))
+
+                        for(k in 0 .. 3) {
+                            if (dateTimeline[k].time <= date) {
+                                chartsData[j][k].add(
+                                    Entry(
+                                        index.toFloat(),
+                                        lastVal[j]
+                                    )
+                                )
+                            }
+                        }
+                    }
+                    documents.add(index, value)
+                    increaser.time = Date(date.time/1000*1000)
+                    increaser.add(Calendar.DATE, 1)
+                    previousDate = increaser.time
+                    index++
                 }
-                setData(entries)
-            }
-            override fun onFailure(exception: Exception) {
-                Log.e("Error", "${exception.toString()}")
+                setData(chartsData[currType][currSize])
             }
 
+            override fun onFailure(exception: Exception) {
+                Log.e("Error", "${exception.stackTrace}")
+            }
         })
     }
+
     fun getDateWithoutTime(date: Date): String {
         val formatter = SimpleDateFormat("EEEE, MMMM dd, yyyy", Locale.getDefault())
         return formatter.format(date)
